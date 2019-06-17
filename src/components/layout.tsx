5d9ac77034932a66f64ff17606b314ff678fbbbe
gmferland <gmferland@gmail.com>
2019-06-16 22:25:36 -0400

    typescript + eslint

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,14 +5,17 @@
  * See: https://www.gatsbyjs.org/docs/static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
 
-import Header from "./header"
-import "./layout.css"
+import Header from './header';
+import './layout.css';
 
-const Layout = ({ children }) => (
+interface ILayoutProps {
+  children: Array<JSX.Element>;
+}
+
+const Layout = (props: ILayoutProps) => (
   <StaticQuery
     query={graphql`
       query SiteTitleQuery {
@@ -24,30 +27,29 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <>
+      <React.Fragment>
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
           style={{
-            margin: `0 auto`,
+            margin: '0 auto',
             maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
+            padding: '0px 1.0875rem 1.45rem',
             paddingTop: 0,
           }}
         >
-          <main>{children}</main>
+          <main>{props.children}</main>
           <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
+            ©
+            {' '}
+            {new Date().getFullYear()}
+, Built with
+            {' '}
             <a href="https://www.gatsbyjs.org">Gatsby</a>
           </footer>
         </div>
-      </>
+      </React.Fragment>
     )}
   />
-)
+);
 
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
-export default Layout
+export default Layout;
